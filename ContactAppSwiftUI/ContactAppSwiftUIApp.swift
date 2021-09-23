a0aec06e9874c7c71592bff7f34294d42b3c6494
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-09-23 20:07:06 +0300

    Initial Commit

---
+++ ContactAppSwiftUI/ContactAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppSwiftUIApp.swift
+//  ContactAppSwiftUI
+//
+//  Created by slava on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
